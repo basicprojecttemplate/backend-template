b48f95cce072273606dc791b50e8a87fc12b0651
vjoshi4 <Vishwanath_joshi@intuit.com>
2020-07-09 13:29:58 +0530

    Adding Basic Spring Boot Project generated from https://start.spring.io/

---
+++ spring-boot/springboot-backend-template/src/test/java/com/basicprojecttemplate/template/springbootbackendtemplate/SpringbootBackendTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basicprojecttemplate.template.springbootbackendtemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBackendTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
